9f5f7c6b58e68a10b00451c97d32e1a71b34ff57
takahirohonda <thon6thon6@hotmail.com>
2024-09-07 00:16:09 +1000

    Update

--- apps/react-app/src/app/app.tsx
+++ apps/react-app/src/app/app.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes, Link } from 'react-router-dom'
 
 export function App() {
-  return <main className="bg-slate-800"></main>
+  return <main className="bg-slate-800">hello</main>
 }
 
 export default App
