798ac3d01aaa4ca1436755a2418c542d04d7a361
takahirohonda <thon6thon6@hotmail.com>
2024-09-25 23:05:54 +1000

    clean up

--- apps/react-app/src/AppRoutes.tsx
+++ apps/react-app/src/AppRoutes.tsx
@@ -1,10 +1,6 @@
 import { Route, Routes, Link } from 'react-router-dom'
-import { SignIn } from './pages/Playground/RestApi/SignIn/SignIn'
-import { SignUp } from './pages/Playground/RestApi/SignUp/SignUp'
-import {
-  PLAYGROUND_REST_API_SIGNIN,
-  PLAYGROUND_REST_API_SIGNUP,
-} from './const/routes'
+import { SignIn } from './pages/Playground/RestApi/TestJwtAuth/SignIn'
+import { PLAYGROUND_REST_API_SIGNIN } from './const/routes'
 import { LayoutMain } from './components/Laytout/LayoutMain'
 
 import { Home } from './pages/Home/Home'
@@ -15,7 +11,6 @@ export const AppRoutes = () => {
       <Route element={<LayoutMain />}>
         <Route path="/" element={<Home />} />
         <Route path={PLAYGROUND_REST_API_SIGNIN} element={<SignIn />} />
-        <Route path={PLAYGROUND_REST_API_SIGNUP} element={<SignUp />} />
       </Route>
     </Routes>
   )
