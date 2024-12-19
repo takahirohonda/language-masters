3f70a95e2349dbeac6ebe9f3393770f0e41a2168
takahirohonda <thon6thon6@hotmail.com>
2024-12-19 15:50:53 +1100

    fix: vite config update

--- apps/react-voice-recorder/vite.config.ts
+++ apps/react-voice-recorder/vite.config.ts
@@ -2,8 +2,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import { VitePWA } from 'vite-plugin-pwa'
+import manifestObj from './public/manifest.json'
 const isGitPageDeploy = Boolean(process.env.GIT_PAGE_DEPLOY)
-
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/react-voice-recorder',
