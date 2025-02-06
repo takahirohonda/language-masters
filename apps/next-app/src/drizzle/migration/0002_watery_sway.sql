ed78e5d9a5fcbd1ac940399e6b9d640731693b8b
Takahiro Honda <43569574+takahirohonda@users.noreply.github.com>
2025-02-06 14:12:06 +1100

    feat: Add Turso support (#4)
    * feat: add turso support
    
    * fixing white line

--- apps/next-app/src/drizzle/migration/0002_watery_sway.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "img_url" text;
--- apps/next-app/src/drizzle/schema.ts
+++ apps/next-app/src/drizzle/schema.ts
@@ -1,25 +1,27 @@
-import { pgTable, text } from 'drizzle-orm/pg-core'
-import { randomUUID } from 'crypto'
 import { sql } from 'drizzle-orm'
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 
-const id = () =>
-  text('id')
-    .primaryKey()
-    .$default(() => randomUUID())
-
-const createdAt = () =>
-  text('created_at')
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull()
-
-export const users = pgTable('users', {
-  id: id(),
-  createdAt: createdAt(),
-  firstName: text('firstname').notNull(),
-  lastName: text('lastname').notNull(),
-  middleName: text('middlename'),
-  displayName: text('display_name'),
+export const usersTable = sqliteTable('users', {
+  id: integer('id').primaryKey(),
+  name: text('name').notNull(),
+  age: integer('age').notNull(),
   email: text('email').unique().notNull(),
-  defaultLanguage: text('default_language'),
-  imageUrl: text('image_url'),
 })
+export const postsTable = sqliteTable('posts', {
+  id: integer('id').primaryKey(),
+  title: text('title').notNull(),
+  content: text('content').notNull(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => usersTable.id, { onDelete: 'cascade' }),
+  createdAt: text('created_at')
+    .default(sql`(CURRENT_TIMESTAMP)`)
+    .notNull(),
+  updateAt: integer('updated_at', { mode: 'timestamp' }).$onUpdate(
+    () => new Date()
+  ),
+})
+export type InsertUser = typeof usersTable.$inferInsert
+export type SelectUser = typeof usersTable.$inferSelect
+export type InsertPost = typeof postsTable.$inferInsert
+export type SelectPost = typeof postsTable.$inferSelect
