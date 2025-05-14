21a45d639f48763a22d59f084882d0b7b58af123
takahirohonda <thon6thon6@hotmail.com>
2025-05-14 10:27:33 +1000

    feat: Using controlled and uncontrolled fields when the form value is number

--- apps/react-app/src/pages/Playground/TestReactHookForm/TestReactHookForm.tsx
+++ apps/react-app/src/pages/Playground/TestReactHookForm/TestReactHookForm.tsx
@@ -28,7 +28,7 @@ export const TestReactHookForm = () => {
 
   const submitHandler = useCallback(async (values: TestFormValues) => {
     await simulateAjaxCall({ data: values })
-    console.log(`Post request with ${JSON.stringify(values)}`)
+    alert(`Post request with ${JSON.stringify(values)}`)
   }, [])
 
   return (
@@ -37,21 +37,47 @@ export const TestReactHookForm = () => {
         onSubmit={handleSubmit(submitHandler)}
         className="flex flex-col gap-[20px] w-[50%] mx-auto"
       >
+        {/* using controller to have the value as number */}
+        <Controller
+          name="age"
+          control={control}
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <FormControl fullWidth>
+              <InputLabel id="age-label">Age</InputLabel>
+              <Select
+                labelId="age-label"
+                id="age-select"
+                label="Age"
+                error={Boolean(errors.age)}
+                onChange={onChange}
+                onBlur={onBlur}
+              >
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={20}>20</MenuItem>
+                <MenuItem value={30}>30</MenuItem>
+              </Select>
+              {Boolean(errors.age as FieldError) && (
+                <FormHelperText error>{errors.age?.message}</FormHelperText>
+              )}
+            </FormControl>
+          )}
+        />
+        {/* We can set the value as string and use this field as uncontrolled input */}
         <FormControl fullWidth>
-          <InputLabel id="age-label">Age</InputLabel>
+          <InputLabel id="age2-label">Age2</InputLabel>
           <Select
-            labelId="age-label"
-            id="age-select"
-            label="Age"
-            error={Boolean(errors.title)}
-            {...register('age')}
+            labelId="age2-label"
+            id="age2-select"
+            label="Age2"
+            error={Boolean(errors.age2)}
+            {...register('age2')}
           >
-            <MenuItem value={10}>10</MenuItem>
-            <MenuItem value={20}>20</MenuItem>
-            <MenuItem value={30}>30</MenuItem>
+            <MenuItem value="10">10</MenuItem>
+            <MenuItem value="20">20</MenuItem>
+            <MenuItem value="30">30</MenuItem>
           </Select>
-          {Boolean(errors.age as FieldError) && (
-            <FormHelperText error>{errors.age?.message}</FormHelperText>
+          {Boolean(errors.age2 as FieldError) && (
+            <FormHelperText error>{errors.age2?.message}</FormHelperText>
           )}
         </FormControl>
         <TextField
--- apps/react-app/src/pages/Playground/TestReactHookForm/TestReactHookForm.types.ts
+++ apps/react-app/src/pages/Playground/TestReactHookForm/TestReactHookForm.types.ts
@@ -2,8 +2,14 @@ import { z } from 'zod'
 import { isNumber } from './formValidation'
 
 export const testFormValueSchema = z.object({
-  age: z.string().min(1, { message: 'Age required' }).refine(isNumber, {
-    message: 'Age needs to be number',
+  age: z.coerce
+    .number({
+      message: 'Age needs to be number',
+    })
+    .int()
+    .min(1, { message: 'Age required' }),
+  age2: z.string().min(1, { message: 'Age2 required' }).refine(isNumber, {
+    message: 'age has to be a number',
   }),
   title: z.string().min(1, { message: 'Title is required' }),
   phone: z.string(),
